1d623325dba97cae5f976d9af0aa5b2a69a15858
Fellipe Amorim <fellipedf@gmail.com>
2018-11-21 23:20:59 -0200

    tipos de eventos

--- exercicios/primeirobot.js
+++ exercicios/primeirobot.js
@@ -8,4 +8,14 @@ bot.start(ctx => {
     ctx.reply(`Seja bem vindo, ${from.first_name}`)
 })
 
+bot.on('text', async (ctx, next) => {
+    await ctx.reply('Eu sou um bot que vou te ajudar a ganhar na kina!')
+    next()
+})
+
+bot.on('text', async ctx => {
+    await ctx.reply('Espero ganharmos o mais rápido possível')
+})
+
+
 bot.startPolling()
